73b5b8c66366a9d6ecb632e75003536e66df19aa
lbcanfield <lewie.b.canfield@gmail.com>
2022-11-12 10:11:39 -0600

    App.js - initial values for the form declared using useState hook. - Form component called within the App component. Form.js - initial construction of the form created.

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,11 +1,22 @@
+import React, { useState } from 'react';
+import Form from './components/Form';
 import './App.css';
 
-function App() {
+// Initial Values for the Form
+const initialFormValues = {
+  name: '',
+  email: '',
+  role: ''
+};
+
+export default function App() {
+
+  // Initial variable state values 
+  const [formValues, setFormValues] = useState(initialFormValues);
   return (
     <div className="App">
-
+      <Form />
     </div>
   );
 }
 
-export default App;
---
+++ team-builder/src/components/Form.js
@@ -0,0 +1,9 @@
+
+
+export default function Form(props) {
+    return (
+        <form className='team-form-container'>
+            Here be the form
+        </form>
+    )
+}
